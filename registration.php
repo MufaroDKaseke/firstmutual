0930f5b14080c39ba5b61fbd5faa35b07fff24b3
MufaroDKaseke <mufarodkaseke@gmail.com>
2024-09-19 15:37:43 +0200

    Added registration form

---
+++ registration.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./dist/lib/fontawesome/css/all.min.css">
+  <link rel="stylesheet" href="./node_modules/animate.css/animate.min.css">
+  <link rel="stylesheet" href="./dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./dist/css/style.min.css">
+</head>
+<body class="bg-primary">
+
+
+
+<main>
+  <section class="register">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-6">
+          <div class="register-form p-3 bg-white border-3">
+            <h4 class="text-center">User Registration</h4>
+            <form action="#">
+              <label for="firstname" class="form-label">Firstname</label>
+              <div class="input-group">
+                <input type="text" name="firstname" id="firstname" class="form-control" placeholder="Firstname">
+              </div>
+              <div class="input-group my-3 justify-content-end">
+                <button class="btn btn-primary">Complete Registration</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+  
+
+  <script src="./node_modules/jquery/dist/jquery.min.js"></script>
+  <script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="./dist/js/main.js"></script>
+</body>
+</html>
